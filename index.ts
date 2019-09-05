7247d8ae260a59214a91760877f435c14b62bf6e
Benson Chen <tumlivein@gmail.com>
2019-09-05 09:37:04 +0800

    7. Catching Errors with TypeScript

--- index.ts
+++ index.ts
@@ -2,16 +2,23 @@ import axios from "axios";
 
 const url = "http://jsonplaceholder.typicode.com/todos/1";
 
-axios.get(url).then(response => {
-  const todo = response.data;
+// interface is used to defined the structure of an object
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
-  const ID = todo.ID;
-  const title = todo.Title;
-  const finished = todo.finished;
+axios.get(url).then(response => {
+  const todo = response.data as Todo;
+
+  const ID = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
 
   console.log(`
     The Todo with ID: ${ID}
     Has a title of: ${title}
-    Is it finished? ${finished}
+    Is it finished? ${completed}
   `);
 });
