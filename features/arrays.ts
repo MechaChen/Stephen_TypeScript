838e46f17db6a8bcc923741db0a225be8cebebfa
Benson Chen <tumlivein@gmail.com>
2019-09-23 11:15:57 +0800

    30. Why Typed Arrays?

--- features/arrays.ts
+++ features/arrays.ts
@@ -11,3 +11,15 @@ const carsByMake = [
 // type Annotation
 const carMakers1: string[] = [];
 const carsByMake1: string[][] = [];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
