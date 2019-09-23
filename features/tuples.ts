183183b19d37f867d49151df3e74a7f1b8d2b7d4
Benson Chen <tumlivein@gmail.com>
2019-09-23 11:36:15 +0800

    35. Why Tuples

--- features/tuples.ts
+++ features/tuples.ts
@@ -12,3 +12,10 @@ type Drink = [string, boolean, number];
 const pepsi1: Drink = ['brown', true, 40];
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
+
+const carSpecs: [number, number] = [400, 3354]; // no meaning
+
+const carStats = {
+  horsepower: 400,
+  weight: 3354,
+};
