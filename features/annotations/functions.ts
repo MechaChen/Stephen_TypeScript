a367ed7c6126f091b13693b53a32071e7838996a
Benson Chen <tumlivein@gmail.com>
2019-09-23 10:36:54 +0800

    25. Annotations for Anonymous Functions

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -2,11 +2,14 @@ const add = (a: number, b: number) => {
   return a + b;
 };
 
-// Return void, but we want return a number acutally
-// const subtract = (a: number, b: number) => {
-//   a - b;
-// }
-
 const subtract = (a: number, b: number): number => {
   return a - b;
 };
+
+function divide(a: number, b: number): number {
+  return a / b;
+};
+
+const multiple = function(a: number, b: number): number {
+  return a * b;
+}
