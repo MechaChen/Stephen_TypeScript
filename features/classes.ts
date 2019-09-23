cabc37fb7f230eed3c09557a038380f239d1110b
Benson Chen <tumlivein@gmail.com>
2019-09-23 14:06:42 +0800

    45. Instance Method Modifiers: Private

--- features/classes.ts
+++ features/classes.ts
@@ -1,19 +1,24 @@
 class Vehicle {
-  drive(): void {
-    console.log('chugga chugga');
-  }
+  // public drive(): void {
+  //   console.log('chugga chugga');
+  // }
 
-  honk(): void {
+  public honk(): void {
     console.log('beep');
   }
 };
 
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
     console.log('vroom');
   };
+
+  startDrivingProcess(): void {
+    this.drive();
+  }
 };
 
 const car = new Car();
-car.drive();
+// car.drive();
+car.startDrivingProcess();
 car.honk();
