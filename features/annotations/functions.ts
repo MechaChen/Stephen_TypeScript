904b741a6e549def468d1cc7dbdc2119ee0d5f9a
Benson Chen <tumlivein@gmail.com>
2019-09-23 10:34:41 +0800

    24. Inference Around Functions

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -1,9 +1,12 @@
-// Type Annotations for functions :
-//   Code we add to tell Typescript what type of arguments a function will receive and what type of values it will return
-
-// Type Inference for functions :
-//   Typescrip tries to figure out what type of value a function will return
-
-const add = (a: number, b: number): number => {
+const add = (a: number, b: number) => {
   return a + b;
+};
+
+// Return void, but we want return a number acutally
+// const subtract = (a: number, b: number) => {
+//   a - b;
+// }
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
 };
