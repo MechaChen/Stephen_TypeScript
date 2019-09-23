728bbaf833feef455499be1f6f01589ab9deb4fb
Benson Chen <tumlivein@gmail.com>
2019-09-23 10:45:34 +0800

    26. Void and Never

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -13,3 +13,30 @@ function divide(a: number, b: number): number {
 const multiple = function(a: number, b: number): number {
   return a * b;
 }
+
+const looger = (message: string): void => {
+  console.log(message);
+
+  // return null; => OK
+  // return undefined; => OK
+  // return 'asdlkfjiower' => Error
+};
+
+
+// 'never' type doesn't expect to have any return value
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const throwError1 = (message: string): string => {
+  if(!message) {
+    throw new Error(message);
+  }
+  return message;
+}
+
+const throwError2 = (message: string): void => {
+  if(!message) {
+    throw new Error(message);
+  }
+}
