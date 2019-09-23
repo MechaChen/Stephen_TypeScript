d217cfffe7c8197358c05b99bf9d7a9c2fa0ffb6
Benson Chen <tumlivein@gmail.com>
2019-09-23 10:29:09 +0800

    23. More on Annotations Around Functions

---
+++ features/annotations/functions.ts
@@ -0,0 +1,9 @@
+// Type Annotations for functions :
+//   Code we add to tell Typescript what type of arguments a function will receive and what type of values it will return
+
+// Type Inference for functions :
+//   Typescrip tries to figure out what type of value a function will return
+
+const add = (a: number, b: number): number => {
+  return a + b;
+};
