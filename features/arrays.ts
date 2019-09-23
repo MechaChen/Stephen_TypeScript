e9a3bd3f41ddda17253d36d5f28a84c65ab50ed2
Benson Chen <tumlivein@gmail.com>
2019-09-23 11:08:50 +0800

    29. Arrays in TypeScript

---
+++ features/arrays.ts
@@ -0,0 +1,13 @@
+// type Inference
+const carMakers = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+const carsByMake = [
+  ['f150'],
+  ['corolla'],
+  ['camaro'],
+];
+
+// type Annotation
+const carMakers1: string[] = [];
+const carsByMake1: string[][] = [];
